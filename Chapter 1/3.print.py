fa0305408cf9e0b9018a310a7218078f1884e8c8
黄兆琦 <1361001127@qq.com>
2024-06-07 16:13:49 +0800

    feat: print函数基本用法

--- Chapter 1/3.print.py
+++ Chapter 1/3.print.py
@@ -1,11 +1,13 @@
-# 任务一：打印数字2024
-print(2024)
+year = 2024  # 年份
+month = 6
+day = 7
+week = "五"
+weather = '阴'
+temperature = 24.2
 
-# 任务二：打印字符串 我是mia
 print("我是mia")
-
-# 任务三：创建变量year，值为2024，打印变量year
-year = 2024
-print(year)
-print(year)
-print(year)
+print(year, "年，我要减肥", sep="", end="\n\n")  # sep: 设置打印多个内容的分隔符
+# end: 设置print执行结束后的操作
+print(year, "年，我要读100本书", sep="", end="\n\n")
+print(year, "年，我要去10个城市旅游", sep="", end="\n\n")
+print("今天是 %d 年 %02d 月 %d 日，星期 %s ，天气 %s ，温度 %.1f 度" % (year, month, day, week, weather, temperature))
