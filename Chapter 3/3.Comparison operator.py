b4843a426909b9545f6d91cb6fa041e2f371eeb6
黄兆琦 <1361001127@qq.com>
2024-06-11 13:27:32 +0800

    feat: 比较运算符与逻辑运算符

---
+++ Chapter 3/3.Comparison operator.py
@@ -0,0 +1,8 @@
+print(3 != 3)  # 判断不相等
+print(3 == 2)  # 判断相等
+print(3 >= 2)
+print(3 <= 2)
+print(3.0 == 3)
+print(True == False)
+print("hello" < "hell")  # 字符串的比较运算：每个字符的ASCII码值
+print(ord("h"))  # 获取字符的ASCII码值
