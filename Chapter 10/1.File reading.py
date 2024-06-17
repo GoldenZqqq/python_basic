237600ecb5b42db459363a5f04f4f89da727a1d3
黄兆琦 <1361001127@qq.com>
2024-06-17 09:57:56 +0800

    feat: 文件读取基本操作

---
+++ Chapter 10/1.File reading.py
@@ -0,0 +1,17 @@
+import os
+
+# 打开文件
+# f = open("./Chapter 10/test.txt", encoding="utf-8")  # 相对路径
+path = os.getcwd()
+filename = path + "/Chapter 10/test.txt"
+f = open(filename, mode="r", encoding="utf-8")  # 绝对路径
+# 读取文件
+# context = f.read(5)
+# print(context)
+# context = f.readline()
+# print(context)
+context = f.readlines()
+print(context)
+
+# 关闭文件
+f.close()
