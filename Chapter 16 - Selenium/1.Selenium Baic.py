52bbfbce957d3cb3828c813a77c34afa24496968
黄兆琦 <1361001127@qq.com>
2024-07-05 15:36:42 +0800

    feat: 学习Selenium webdriver基本操作

---
+++ Chapter 16 - Selenium/1.Selenium Baic.py
@@ -0,0 +1,37 @@
+# webdriver的基本操作
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+url = "https://www.baidu.com"
+url1 = "https://jd.com"
+service = Service()
+opt = Options()
+opt.add_experimental_option("detach", True)
+opt.add_argument("--dsiable-blink-features=AutomationControlled")
+browser = webdriver.Chrome(service=service, options=opt)
+browser.maximize_window()
+# browser.set_window_position(100, 20)
+# browser.set_window_size(900, 900)
+browser.get(url)
+
+# time.sleep(1)
+# browser.get(url1)
+
+# time.sleep(1)
+# browser.back()
+
+# time.sleep(1)
+# browser.forward()
+
+# time.sleep(1)
+# browser.refresh()
+
+# time.sleep(1)
+# page_text = browser.page_source
+# print(page_text)
+
+time.sleep(2)
+# browser.close()  # 关闭一个标签
+browser.quit()  # 关闭整个浏览器
