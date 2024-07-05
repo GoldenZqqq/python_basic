323525dc1fe516d4d2b080bcc52400e95e59fc4d
黄兆琦 <1361001127@qq.com>
2024-07-05 16:57:15 +0800

    feat: 学习Selenium网页加载策略与元素等待

---
+++ Chapter 16 - Selenium/3.Element waiting&Loading strategies.py
@@ -0,0 +1,29 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service()
+opt = Options()
+opt.add_experimental_option("detach", True)
+opt.add_argument("--dsiable-blink-features=AutomationControlled")
+opt.page_load_strategy = "none"
+
+url = "https://www.ruike1.com/forum.php?mod=forumdisplay&fid=47&filter=author&orderby=dateline"
+browser = webdriver.Chrome(service=service, options=opt)
+# browser.implicitly_wait(5)
+start = time.time()
+browser.get(url)
+end = time.time()
+print(f"打开页面的时间：{end - start}")
+
+WebDriverWait()
+next_page = browser.find_element(By.XPATH, '//*[@id="fd_page_bottom"]/div/a[1]')
+next_link = next_page.get_attribute("href")
+print(f"下一页链接：{next_link}")
+next_page.click()
+
+browser.close()
