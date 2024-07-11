eae5c52cf4cb3887a4c672300d5b0b762c6fca27
黄兆琦 <1361001127@qq.com>
2024-07-11 08:57:04 +0800

    feat: 学习Selenium窗口切换功能

---
+++ Chapter 16 - Selenium/4.Switch window.py
@@ -0,0 +1,45 @@
+# 切换窗口
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service()
+opt = Options()
+opt.add_experimental_option("detach", True)
+opt.add_argument("--dsiable-blink-features=AutomationControlled")
+opt.page_load_strategy = "eager"
+browser = webdriver.Chrome(service=service, options=opt)
+
+url = "https://www.ruike1.com/forum.php?mod=forumdisplay&fid=47&filter=author&orderby=dateline"
+browser.get(url)
+
+time.sleep(3)
+# //table[@id="threadlisttableid"]/tbody[3]/tr/th/a[2]
+# //table[@id="threadlisttableid"]/tbody[4]/tr/th/a[2]
+# //table[@id="threadlisttableid"]/tbody[5]/tr/th/a[2]
+locator = (By.XPATH, '//table[@id="threadlisttableid"]/tbody[3]/tr/th/a[2]')
+WebDriverWait(browser, 5).until(EC.presence_of_all_elements_located(locator))
+
+next_page = browser.find_element(
+    By.XPATH, '//table[@id="threadlisttableid"]/tbody[3]/tr/th/a[2]'
+)
+for next in next_page:
+    next.click()
+    time.sleep(3)
+    filename = browser.find_element(By.XPATH, '//*[@id="thread_subject"]').text
+    break
+
+""" 
+# 浏览器窗口切换
+driver.switch_to.window(driver.window_handles[0])  # 切换到第一个窗口
+
+# 打开一个新的页面并切换至新页面
+driver.switch_to.new_window('tab')
+
+# 打开一个新的窗口并切换至新窗口
+driver.switch_to.new_window('window')
+"""
